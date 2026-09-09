78668872eb8fdf9d95a6028e8ee5911568ca0d56
Lnilpe <ducenkokirill474@gmail.com>
2026-09-09 13:40:54 +0300

    B1

--- proverka.py
+++ proverka.py
@@ -1,32 +1,19 @@
-def make_phone(raw):
-    try:
-        # Приводим к строке
-        text = str(raw)
-        
-        # Проверяем, что все символы — цифры
-        if not text.isdigit():
-            return None
-        
-        # Проверяем длину (не больше 11 цифр)
-        if len(text) > 11:
-            return None
-        
-        # Если всё ок — добавляем + и возвращаем
-        number = "+" + text
-        return number
-        
-    except Exception as e:
-        print(f"ошибка: {e}")
-        return None
-        
-    finally:
-        print("проверка номера завершена")
+def rub_word(n):
+    last_two = n % 100
+    last_one = n % 10
+    #дальше по ТЗ
+    if 11 <= last_two <= 14:
+        word = "рублей"
+    elif last_one == 1:
+        word = "рубль"
+    elif 2 <= last_one <= 4:
+        word = "рубля"
+    else:
+        word = "рублей"
+    return word
 
-
-# Тесты
-print(make_phone("79995553435"))   # +79995553435
-print(make_phone("123"))           # +123
-print(make_phone("123456789012"))  # None (12 цифр > 11)
-print(make_phone("7999a553435"))   # None (есть буква)
-print(make_phone(79995553435))     # +79995553435 (работает с int)
-print(make_phone("+79995553435"))  # None (есть +)
+try:
+    money = int(input("Сумма: "))
+    print(money, rub_word(money))
+except ValueError:
+    print("Нужно целое число")
